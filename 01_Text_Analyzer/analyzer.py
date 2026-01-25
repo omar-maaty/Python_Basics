ebe48f2c31483e3483833d238a588199833a2191
Omar Maaty <omarmatty7@gmail.com>
2026-01-25 08:56:39 +0200

    modified text analyzer file

--- 01_Text_Analyzer/analyzer.py
+++ 01_Text_Analyzer/analyzer.py
@@ -1,1 +1,20 @@
-print('hello world')
+fname=input('Enter the file path: ')
+counts={}
+try:
+    with open(fname) as fopen:
+        for line in fopen:
+            words=line.split()
+            for word in words:
+                word=word.lower()
+                counts[word]=counts.get(word,0)+1
+    lst=[(v,k) for k,v in counts.items()]
+    lst=sorted(lst,reverse=True)
+    print('Top most frequent words:')
+    x=1
+    for (v,k) in lst[:5]:
+        print(f'{x}.{k} ({v})')
+        x +=1
+
+except:
+    print('File not founded')
+
